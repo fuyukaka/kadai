db78c42f594257bffb1001047d6cfc326e2f9f47
fuyukaka <fuyukaka@fuyukaka-001.fuji.fsi.local>
2017-06-20 16:12:59 +0900

    変更

--- change.php
+++ change.php
@@ -3,11 +3,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>変更画面</title>
+<title>変更結果画面</title>
 </head>
 <body>
 
 <?php
+$connect=pg_connect("host=localhost dbname=postgres port=5433 user=postgres password=fsiabc3150");
+
+$sql="INSERT INTO my_items(id,item_name,price,keyword) VALUES($id,'$item_name',$price,'$keyword')";
+
+$done=pg_query($connect,$sql);
 ?>
 
 </body>
---
+++ changeForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>変更画面</title>
+</head>
+<body>
+<form action="newdate.php"method="post">
+
+ID
+<br><input type="text"name="id"value=""/>
+<br><br>商品名
+<br><input type="text"name="my_item"value=""/>
+<br><br>価格
+<br><input type="text"name="price"value=""/>
+<br><br>キーワード
+<br><input type="text"name="keyword"value=""/>
+
+<br><br><input type="submit"value="送信する"/>
+</form>
+</body>
+</html>
